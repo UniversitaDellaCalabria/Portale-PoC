a01ad2d6aaa7c33a8e722b8937d2d86955c9e570
peppelinux <giuseppe.demarco@unical.it>
2020-11-13 15:01:08 +0100

    minor generalizations

--- unicms/cms/settings.py
+++ unicms/cms/settings.py
@@ -9,13 +9,13 @@ CMS_IMAGE_CATEGORY_SIZE = 128
 CMS_IMAGE_THUMBSIZE = 128
 
 CMS_PUBLICATION_PREFIX_RESERVED_WORD = 'content/posts/'
-CMS_PUBLICATION_URL_VIEW_REGEXP = f'(?:[\/a-zA-Z0-9\.\-\_]*)({CMS_PUBLICATION_PREFIX_RESERVED_WORD})(?P<slug>[a-z0-9\-]*)'
+CMS_PUBLICATION_URL_VIEW_REGEXP = f'(?P<context>[\/a-zA-Z0-9\.\-\_]*)({CMS_PUBLICATION_PREFIX_RESERVED_WORD})(?P<slug>[a-z0-9\-]*)'
 
-CMS_APP_REGEXP_URLPATH = {
+CMS_APP_REGEXP_URLPATHS = {
     'cms.handlers.ContentViewHandler' : CMS_PUBLICATION_URL_VIEW_REGEXP,
 }
 
-# re.match(CMS_PUBLICATION_URL_REGEXP, '/blah/blah/content/post/yessa-man-again-20')
+# re.match(CMS_PUBLICATION_URL_REGEXP, '/content/posts/content/post/yessa-man-again-20')
 
 # .groups()
 # Out[29]: ('content/post/', 'yessa-man-again-20')
