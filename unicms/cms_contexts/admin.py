69160fa6760abb6f16dcc56ffb5cd8bee6f585d4
peppelinux <giuseppe.demarco@unical.it>
2020-11-24 12:14:05 +0100

    draft fix on publish

--- unicms/cms_contexts/admin.py
+++ unicms/cms_contexts/admin.py
@@ -9,6 +9,7 @@ from django.http import (HttpResponse,
                          HttpResponseBadRequest,
                          HttpResponseRedirect)
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext, gettext_lazy as _
 
@@ -36,7 +37,8 @@ class AbstractPreviewableAdmin(admin.ModelAdmin):
             for attr in "id pk _state created_by modified_by created modified".split(' '):
                 if draft.get(attr):
                     draft.pop(attr)
-                
+            
+            draft['date_start'] = timezone.localtime()
             new_obj = obj.__class__.objects.create(**draft)
             tags = [i for i in obj.tags.values_list('name', flat=1)]
             new_obj.tags.add(*tags)
@@ -47,7 +49,7 @@ class AbstractPreviewableAdmin(admin.ModelAdmin):
             return HttpResponseRedirect(url)  
         
         elif request.POST.get('state') == 'published' and obj.draft_of:
-            published = obj.__class__.filter(pk=obj.draft_of).first()
+            published = obj.__class__.objects.filter(pk=obj.draft_of).first()
             if not published:
                 self.message_user(request, 
                                   "Draft missed its parent page ... ",
