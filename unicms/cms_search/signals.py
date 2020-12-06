fe8935bf72a4ab059d6b9111c22dc7ed643db120
peppelinux <giuseppe.demarco@unical.it>
2020-12-06 18:52:54 +0100

    clean up

--- unicms/cms_search/signals.py
+++ unicms/cms_search/signals.py
@@ -1,9 +1,6 @@
-from django.contrib.auth.models import User
+from cms.utils import load_hooks
 from django.db.models.signals import (pre_save, post_save,
                                       pre_delete, post_delete)
-
-from cms.models import Page, Publication
-from cms.utils import load_hooks
 
 
 def cms_pre_save(instance, *args, **kwargs):
@@ -19,6 +16,8 @@ def cms_post_delete(instance, *args, **kwargs):
     load_hooks(instance, 'POSTDELETE', *args, **kwargs)
 
 
+# all the models will send signals here but ... Only who have some hook
+# registered will turn on the lights
 pre_save.connect(cms_pre_save)
 post_save.connect(cms_post_save)
 pre_delete.connect(cms_pre_delete)
