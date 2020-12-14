2e0dad6786ea11faf88a4ee04b4e60b6d435c5e6
peppelinux <giuseppe.demarco@unical.it>
2020-12-14 11:18:33 +0100

    included untracked files

---
+++ unicms/cms/templates/utils.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import re
+
+from django.conf import settings
+from django.template.backends.django import DjangoTemplates
+
+
+logger = logging.getLogger(__name__)
+CMS_TEMPLATE_PATH_PREFIX = ''
+
+
+def is_dir_usefull(dir_path):
+    if os.path.isdir(dir_path) and \
+       os.listdir(dir_path):
+        return True
+
+
+def get_unicms_templates():
+    templates = []
+    for te in settings.TEMPLATES:
+        te['NAME'] = 'fake name'
+        te.pop('BACKEND')
+        td = DjangoTemplates(te)
+        for dir_path in td.template_dirs:
+            node = f'{dir_path}/{CMS_TEMPLATE_PATH_PREFIX}'
+            if not is_dir_usefull(node): continue
+            for i in ('pages', 'blocks'):
+                node2 = f'{node}{i}'
+                node_els = [node, f'{i}/']
+                if not is_dir_usefull(node2): continue
+                elements = [(node_els[0], node_els[1], e) 
+                            for e in os.listdir(node2)
+                            if re.findall('.html$', e)]
+                templates.extend(elements)
+    _msg = 'uniCMS Template loader - found: {}'
+    logger.debug(_msg.format(templates))
+    return sorted(templates)
